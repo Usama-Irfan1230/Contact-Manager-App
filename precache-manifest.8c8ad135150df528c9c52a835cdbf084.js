7123818178e1faa33b52b7d2e3159cdcd82ce522
Usama Irfan <irfan.du20@gmail.com>
2020-05-17 09:10:08 +0500

    Updates

---
+++ precache-manifest.8c8ad135150df528c9c52a835cdbf084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33c5d4b5d5e4b73c2a50d675d0f6993",
+    "url": "/Contact-Manager-App/index.html"
+  },
+  {
+    "revision": "d6476737c2b06ddf5814",
+    "url": "/Contact-Manager-App/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e43ce8352ff719181422",
+    "url": "/Contact-Manager-App/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d6476737c2b06ddf5814",
+    "url": "/Contact-Manager-App/static/js/2.f9e5f982.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager-App/static/js/2.f9e5f982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e43ce8352ff719181422",
+    "url": "/Contact-Manager-App/static/js/main.01f1a797.chunk.js"
+  },
+  {
+    "revision": "c4759fbbc4a17ab2cca8",
+    "url": "/Contact-Manager-App/static/js/runtime-main.69b9e4ae.js"
+  }
+]);
